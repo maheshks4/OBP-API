408929d8fc13dcaedd8f8bd855c7ceb288d6bec1
mahesh <mahesh.ks4@gmail.com>
2017-06-04 18:55:01 +0530

    first version OBP experiment

---
+++ ConsoleApplication1/OBPHelper.cs
@@ -0,0 +1,268 @@
+﻿using RestSharp.Extensions.MonoHttp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace ConsoleApplication1
+{
+    public class OBPHelper
+    {
+            string oauth_callback = "http://www.bbc.co.uk/news";
+            // Use your own callback URL
+            string oauth_consumer_key = "q4vfomz3peqmtoxg0dgs0mg3aafa22a0ku3yei2v";
+            // Use your own oauth_consumer_key
+            string oauth_nonce = Guid.NewGuid().ToString();
+            // Use your own oauth_nonce
+            string oauth_signature = "";
+            // Leave this blank for now
+            string oauth_signature_method = "HMAC-SHA1";
+            // "HMAC-SHA1" or "HMAC-SHA256"
+   
+            string oauth_token_secret = null;
+
+            string oauth_consumer_secret = "xygpxhs01smnphljh32p4n3b3oud0jclzoy3stbq";
+            // Use your own oauth_consumer_secret
+
+            string uri = "https://apisandbox.openbankproject.com/oauth/initiate";
+
+            string authorizationstring = null;
+
+        //step1
+            public Dictionary<string, string> ObtainRequestToken(string oauth_callback, string oauth_consumer_key, string oauth_nonce,
+                   string oauth_consumer_secret, string uri)
+            {
+                authorizationstring = ProcessRequest(oauth_callback, oauth_consumer_key, oauth_nonce,
+                 oauth_consumer_secret, null, uri, null, null, "POST");
+
+                return ParseHtmlResponse(MakeHttpCall(uri, authorizationstring,"POST"));
+            }
+
+        //step3
+            public Dictionary<string, string> ConvertRequestTokentoAccessToken(string oauth_callback, string oauth_consumer_key, string oauth_nonce,
+                      string oauth_consumer_secret, string oauth_token_secret, string uri, string oauth_verifier, string oauth_token)
+            {
+                 authorizationstring = ProcessRequest(oauth_callback, oauth_consumer_key, oauth_nonce,
+                 oauth_consumer_secret, oauth_token_secret, uri, oauth_verifier, oauth_token, "POST");
+
+                 return ParseHtmlResponse(MakeHttpCall(uri, authorizationstring, "POST"));
+            }
+
+
+        //step4 and 5
+            public string AccessingProtectedResources(string oauth_callback, string oauth_consumer_key, string oauth_nonce,
+                          string oauth_consumer_secret, string oauth_token_secret, string uri, string oauth_verifier, string oauth_token, string method)
+            {
+                authorizationstring = ProcessRequest(oauth_callback, oauth_consumer_key, oauth_nonce,
+                 oauth_consumer_secret, oauth_token_secret, uri, oauth_verifier, oauth_token, method);
+
+                if (method == "GET")
+                    return MakeHttpCall(uri, authorizationstring, method);
+                else
+                {
+                    string json = "{\"bank_id\":\"rbs\",\"account_id\":\"reallypoor\",\"amount\":\"1.00\"}";
+                    return MakeJsonHttpCall(uri, authorizationstring, method, json);
+                }
+            }
+               
+
+
+
+        private string ProcessRequest(string oauth_callback, string oauth_consumer_key, string oauth_nonce, 
+                string oauth_consumer_secret, string oauth_token_secret, string uri, string oauth_verifier, string oauth_token, string method)
+        {
+            this.oauth_callback =  oauth_callback;
+            // Use your own callback URL
+            this.oauth_consumer_key = oauth_consumer_key;
+            // Use your own oauth_consumer_key
+            this.oauth_nonce = oauth_nonce;
+           
+            // Leave this blank for now
+            this.oauth_signature_method = "HMAC-SHA1";
+            // "HMAC-SHA1" or "HMAC-SHA256"
+            string oauth_timestamp = "1496490661";
+            // Use your own oauth_timestamp
+            string oauth_version = "1";
+            // "1.0" or "1"
+
+            this.oauth_token_secret = oauth_token_secret;
+
+            this.oauth_consumer_secret = oauth_consumer_secret;
+            // Use your own oauth_consumer_secret
+
+            
+            this.uri = uri;
+
+            // Create a list of OAuth parameters
+            List<KeyValuePair<string, string>> oauthparameters = new List<KeyValuePair<string, string>>();
+
+            //step1
+            if (oauth_callback != null)
+                oauthparameters.Add(new KeyValuePair<string, string>("oauth_callback", oauth_callback));
+            
+            //step3
+            if (oauth_verifier != null && oauth_token!= null)
+            {
+                oauthparameters.Add(new KeyValuePair<string, string>("oauth_verifier", oauth_verifier));
+                oauthparameters.Add(new KeyValuePair<string, string>("oauth_token", oauth_token));
+            }
+
+            //step4 & 5
+            if (oauth_verifier == null && oauth_token != null)
+            {                
+                oauthparameters.Add(new KeyValuePair<string, string>("oauth_token", oauth_token));
+            }
+            
+           
+
+            oauthparameters.Add(new KeyValuePair<string, string>("oauth_consumer_key", oauth_consumer_key));
+            oauthparameters.Add(new KeyValuePair<string, string>("oauth_nonce", oauth_nonce));
+            oauthparameters.Add(new KeyValuePair<string, string>("oauth_signature_method", oauth_signature_method));
+            oauthparameters.Add(new KeyValuePair<string, string>("oauth_timestamp", oauth_timestamp));
+            oauthparameters.Add(new KeyValuePair<string, string>("oauth_version", oauth_version));
+
+            // Sort the OAuth parameters on the key
+            oauthparameters.Sort((x, y) => x.Key.CompareTo(y.Key));
+
+            // Construct the Base String
+            string basestring = method.ToUpper() + "&" + HttpUtility.UrlEncode(uri) + "&";
+            foreach (KeyValuePair<string, string> pair in oauthparameters)
+            {
+                basestring += pair.Key + "%3D" + HttpUtility.UrlEncode(pair.Value) + "%26";
+            }
+            basestring = basestring.Substring(0, basestring.Length - 3);
+            //Gets rid of the last "%26" added by the foreach loop
+
+            // Makes sure all the Url encoding gives capital letter hexadecimal 
+            // i.e. "=" is encoded to "%3D", not "%3d"
+            char[] basestringchararray = basestring.ToCharArray();
+            for (int i = 0; i < basestringchararray.Length - 2; i++)
+            {
+                if (basestringchararray[i] == '%')
+                {
+                    basestringchararray[i + 1] = char.ToUpper(basestringchararray[i + 1]);
+                    basestringchararray[i + 2] = char.ToUpper(basestringchararray[i + 2]);
+                }
+            }
+            basestring = new string(basestringchararray);
+
+            oauth_signature = GetOauth_Signature(basestring, oauth_signature_method, oauth_consumer_secret, oauth_token_secret);
+
+            // Create the Authorization string for the WebRequest header
+            string authorizationstring = "";
+            foreach (KeyValuePair<string, string> pair in oauthparameters)
+            {
+                authorizationstring += pair.Key;
+                authorizationstring += "=";
+                authorizationstring += pair.Value;
+                authorizationstring += ",";
+            }
+            authorizationstring += "oauth_signature=" + oauth_signature;
+
+            return authorizationstring;
+           
+        }
+
+
+        private string MakeHttpCall(string uri, string authorizationstring, string method)
+        {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Method = method;
+            request.Headers.Add("Authorization", "OAuth " + authorizationstring);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            return responseFromServer;
+           
+        }
+
+
+        private string MakeJsonHttpCall(string uri, string authorizationstring, string method, string json)
+        {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.ContentType = "text/json";
+            request.Method = method;
+            request.Headers.Add("Authorization", "OAuth " + authorizationstring);
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {                
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            return responseFromServer;
+
+        }
+
+        private Dictionary<string, string> ParseHtmlResponse(string responseFromServer)
+        {
+            string[] listParams = responseFromServer.Split('&');
+            Dictionary<string, string> response = new Dictionary<string, string>();
+
+            foreach (string param in listParams)
+            {
+                string[] data = param.Split('=');
+                response.Add(data[0], data[1]);
+            }
+
+            return response;
+        }
+
+
+        private string GetOauth_Signature(string basestring, string oauth_signature_method, string oauth_consumer_secret, string oauth_token_secret)
+        {
+            string oauth_signature = "";
+
+            // Encrypt with either SHA1 or SHA256, creating the Signature
+            var enc = Encoding.ASCII;
+            if (oauth_signature_method == "HMAC-SHA1")
+            {
+                HMACSHA1 hmac = new HMACSHA1(enc.GetBytes(oauth_consumer_secret + "&" + oauth_token_secret ?? string.Empty));
+                hmac.Initialize();
+                byte[] buffer = enc.GetBytes(basestring);
+                string hmacsha1 = BitConverter.ToString(hmac.ComputeHash(buffer)).Replace("-", "").ToLower();
+                byte[] resultantArray = new byte[hmacsha1.Length / 2];
+                for (int i = 0; i < resultantArray.Length; i++)
+                {
+                    resultantArray[i] = Convert.ToByte(hmacsha1.Substring(i * 2, 2), 16);
+                }
+                string base64 = Convert.ToBase64String(resultantArray);
+                oauth_signature = HttpUtility.UrlEncode(base64);
+            }
+            else if (oauth_signature_method == "HMAC-SHA256")
+            {
+                HMACSHA256 hmac = new HMACSHA256(enc.GetBytes(oauth_consumer_secret + "&" + oauth_token_secret ?? string.Empty));
+                hmac.Initialize();
+                byte[] buffer = enc.GetBytes(basestring);
+                string hmacsha256 = BitConverter.ToString(hmac.ComputeHash(buffer)).Replace("-", "")
+                    .ToLower();
+                byte[] resultantArray = new byte[hmacsha256.Length / 2];
+                for (int i = 0; i < resultantArray.Length; i++)
+                {
+                    resultantArray[i] = Convert.ToByte(hmacsha256.Substring(i * 2, 2), 16);
+                }
+                string base64 = Convert.ToBase64String(resultantArray);
+                oauth_signature = HttpUtility.UrlEncode(base64);
+            }
+
+            return oauth_signature;
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp.Extensions.MonoHttp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+// You will have to add the System.Web to your References if you are using a
+// standard console application template.  
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OBPHelper ObtainToken = new OBPHelper();
+
+            //step1
+           /* Dictionary<string,string> token =  ObtainToken.ObtainRequestToken(oauth_callback:"http://www.bbc.co.uk/news", 
+                oauth_consumer_key:"q4vfomz3peqmtoxg0dgs0mg3aafa22a0ku3yei2v",
+                oauth_nonce:Guid.NewGuid().ToString(),
+                oauth_consumer_secret:"xygpxhs01smnphljh32p4n3b3oud0jclzoy3stbq",                 
+                uri:"https://apisandbox.openbankproject.com/oauth/initiate");
+            Debug.WriteLine("oauth_token:" + token["oauth_token"] + " oauth_token_secret:" + token["oauth_token_secret"]);
+            */
+            //oauth_token:IOUAAUIWMTHEW0C0RRQLLWKZTKYRYIIFOTHSJBQL oauth_token_secret:RFCY5KVEFHZFAKIFOLHAQE3HFU4XPJPYTDPW0JMM
+            
+
+            //step2: get oauth_verifier from browser
+            //https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=IOUAAUIWMTHEW0C0RRQLLWKZTKYRYIIFOTHSJBQL
+            //https://apisandbox.openbankproject.com/oauth/thanks?redirectUrl=http%253A%252F%252Fwww.bbc.co.uk%252Fnews%253Foauth_token%253DIOUAAUIWMTHEW0C0RRQLLWKZTKYRYIIFOTHSJBQL%2526oauth_verifier%253D81748
+
+            //step 3 : Converting the request token to an access token
+           /* Dictionary<string, string> token = ObtainToken.ConvertRequestTokentoAccessToken(oauth_callback :null, 
+                oauth_consumer_key:"q4vfomz3peqmtoxg0dgs0mg3aafa22a0ku3yei2v", 
+                oauth_nonce:Guid.NewGuid().ToString(),
+                oauth_consumer_secret:"xygpxhs01smnphljh32p4n3b3oud0jclzoy3stbq", 
+                oauth_token_secret:"RFCY5KVEFHZFAKIFOLHAQE3HFU4XPJPYTDPW0JMM", 
+                uri:"https://apisandbox.openbankproject.com/oauth/token", 
+                oauth_verifier:"81748", 
+                oauth_token: "IOUAAUIWMTHEW0C0RRQLLWKZTKYRYIIFOTHSJBQL");
+            Debug.WriteLine("oauth_token:" + token["oauth_token"] + " oauth_token_secret:" + token["oauth_token_secret"]);
+           */
+            //oauth_token:VQFOSWCWDYDOHRBJGTQSYXAQTXEN4BOZBSDJFTGK oauth_token_secret:S4GJUNYMBIK1ZXLUC1C1WF2V1WORQWYF3PWYMKND
+
+            //step 4 : Accessing protected resources (GET)
+            string Json = ObtainToken.AccessingProtectedResources(oauth_callback: null, 
+                oauth_consumer_key: "q4vfomz3peqmtoxg0dgs0mg3aafa22a0ku3yei2v", 
+                oauth_nonce: Guid.NewGuid().ToString(),
+                oauth_consumer_secret: "xygpxhs01smnphljh32p4n3b3oud0jclzoy3stbq", 
+                oauth_token_secret: "S4GJUNYMBIK1ZXLUC1C1WF2V1WORQWYF3PWYMKND", 
+                uri:"https://apisandbox.openbankproject.com/obp/v1.2.1/accounts/private", 
+                oauth_verifier:null, 
+                oauth_token: "VQFOSWCWDYDOHRBJGTQSYXAQTXEN4BOZBSDJFTGK",
+                method:"GET");
+            Debug.WriteLine(Json);
+            //{"accounts":[{"id":"MAH0001","label":null,"views_available":[{"id":"owner","short_name":"Owner","description":"Owner View","is_public":false,"alias":"","hide_metadata_if_alias_used":false,"can_add_comment":true,"can_add_corporate_location":true,"can_add_image":true,"can_add_image_url":true,"can_add_more_info":true,"can_add_open_corporates_url":true,"can_add_physical_location":true,"can_add_private_alias":true,"can_add_public_alias":true,"can_add_tag":true,"can_add_url":true,"can_add_where_tag":true,"can_delete_comment":true,"can_delete_corporate_location":true,"can_delete_image":true,"can_delete_physical_location":true,"can_delete_tag":true,"can_delete_where_tag":true,"can_edit_owner_comment":true,"can_see_bank_account_balance":true,"can_see_bank_account_bank_name":true,"can_see_bank_account_currency":true,"can_see_bank_account_iban":true,"can_see_bank_account_label":true,"can_see_bank_account_national_identifier":true,"can_see_bank_account_number":true,"can_see_bank_account_owners":true,"can_see_bank_account_swift_bic":true,"can_see_bank_account_type":true,"can_see_comments":true,"can_see_corporate_location":true,"can_see_image_url":true,"can_see_images":true,"can_see_more_info":true,"can_see_open_corporates_url":true,"can_see_other_account_bank_name":true,"can_see_other_account_iban":true,"can_see_other_account_kind":true,"can_see_other_account_metadata":true,"can_see_other_account_national_identifier":true,"can_see_other_account_number":true,"can_see_other_account_swift_bic":true,"can_see_owner_comment":true,"can_see_physical_location":true,"can_see_private_alias":true,"can_see_public_alias":true,"can_see_tags":true,"can_see_transaction_amount":true,"can_see_transaction_balance":true,"can_see_transaction_currency":true,"can_see_transaction_description":true,"can_see_transaction_finish_date":true,"can_see_transaction_metadata":true,"can_see_transaction_other_bank_account":true,"can_see_transaction_start_date":true,"can_see_transaction_this_bank_account":true,"can_see_transaction_type":true,"can_see_url":true,"can_see_where_tag":true}],"bank_id":"hsbc-test"}]}
+
+
+            //step 5 : Accessing protected resources (POST)
+            /*string transcation = ObtainToken.AccessingProtectedResources(oauth_callback: null,
+                oauth_consumer_key: "q4vfomz3peqmtoxg0dgs0mg3aafa22a0ku3yei2v",
+                oauth_nonce: Guid.NewGuid().ToString(),
+                oauth_consumer_secret: "xygpxhs01smnphljh32p4n3b3oud0jclzoy3stbq",
+                oauth_token_secret: "S4GJUNYMBIK1ZXLUC1C1WF2V1WORQWYF3PWYMKND",
+                uri: "https://apisandbox.openbankproject.com/obp/v1.2.1/banks/hsbc-test/accounts/MAH0001/owner/transactions",
+                oauth_verifier: null,
+                oauth_token: "VQFOSWCWDYDOHRBJGTQSYXAQTXEN4BOZBSDJFTGK",
+                method: "POST");
+            Debug.WriteLine(transcation);*/
+
+
+//            string url = "https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=IOUAAUIWMTHEW0C0RRQLLWKZTKYRYIIFOTHSJBQL";
+//WebRequest myReq = WebRequest.Create(url);
+//string credentials = "mahesh.ks4@gmail.com:TestBank1234@";
+//CredentialCache mycache = new CredentialCache();
+//myReq.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes(credentials));
+//WebResponse wr = myReq.GetResponse();
+//Stream receiveStream = wr.GetResponseStream();
+//StreamReader reader = new StreamReader(receiveStream, Encoding.UTF8);
+//string content = reader.ReadToEnd();
+//Console.WriteLine(content);
+//var json = "[" + content + "]"; // change this to array
+//var objects = JArray.Parse(json); // parse as array  
+//foreach (JObject o in objects.Children<JObject>())
+//{
+//    foreach (JProperty p in o.Properties())
+//    {
+//        string name = p.Name;
+//        string value = p.Value.ToString();
+//        Console.Write(name + ": " + value);
+//    }
+//}
+
+     
+
+     
+            
+            Console.ReadLine();
+        }     
+     
+    }
+}
